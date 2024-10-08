b201129164765094cd62b03dd6c71b97b1a33d69
janvii04 <jeevan13798@gmail.com>
2024-10-08 10:53:31 +0530

    task2

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const port = 3003
+
+const dotenv = require("dotenv")
+dotenv.config()
+
+app.use(express.json())
+app.use(express.urlencoded({extented: true}))
+
+
+require('./dbconnection').connectiondb()
+require('./models/index')
+
+const userRoute=require("./routers/userRoute")
+app.use("/users",userRoute)
+
+
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`)
+});
---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+module.exports = (Sequelize, sequelize, DataTyes) =>{
+    return sequelize.define('user', {
+        ...require('./cors')(Sequelize, DataTyes),
+        name: {
+            type: DataTyes.STRING(255),
+            allowNull: false
+        },
+
+        email: {
+            type: DataTyes.STRING(255),
+            allowNull: false
+        },
+
+        gender: {
+            type: DataTyes.STRING(255),
+            allowNull: false
+        },
+
+        age:{
+            type: DataTyes.INTEGER,
+            allowNull: false
+        }
+    },{
+        tableName: 'user'
+    })
+}
